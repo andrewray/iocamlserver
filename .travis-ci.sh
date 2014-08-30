ebc8e45d2f53042628738b693b43445fc6a6acd3
andrewray <evilkidder@gmail.com>
2014-08-30 12:51:00 +0100

    testing travis - use opam from ppa instead

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -9,20 +9,14 @@ case "$OCAML_VERSION,$OPAM_VERSION" in
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
-OPAM=opam-1.2.0-beta4-i686-Linux
-
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
-wget https://github.com/ocaml/opam/releases/download/1.2.0-beta4/$OPAM
-chmod +x $OPAM
 export OPAMYES=1
-ls -la
-pwd
-file $OPAM
-./$OPAM init 
+opam init 
 eval `opam config env`
-./$OPAM remote add iocaml-dev git://github.com/andrewray/opam.git
-./$OPAM update
-./$OPAM install iocaml.999.9.9 
+opam remote add iocaml-dev git://github.com/andrewray/opam.git
+opam update
+opam install iocaml.999.9.9 --deps-only
+make
 
